b7d55c55ad9f0e40b201bdb442ce9d45c210acc5
gurumee <gurumee92@gmail.com>
2020-12-14 22:51:41 +0900

    Update cors

--- src/main/java/com/gurumee/demoapi/configs/SecurityConfig.java
+++ src/main/java/com/gurumee/demoapi/configs/SecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .headers().frameOptions().disable()
                     .and()
